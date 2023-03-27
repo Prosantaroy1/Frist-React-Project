2c42c3dd13bfa23a2c4f7df9dc9cf9c9f910d9b0
Prosantaroy1 <royprosanta172@gmail.com>
2023-03-28 02:51:19 +0600

    completed simple

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,13 +9,13 @@ const Card = ({card}) => {
         totalPrice =totalPrice + product.price;
         totalShipping = totalShipping + product.shipping;
     }
-    const tax= total*7/100;
+    const tax= totalPrice*7/100;
     const GrandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='card'>
             <h4 style={{textAlign: 'center'}}>Order List</h4>
             <p>Seleted Items: {card.length}</p>
-            <p>Total Price: {total}</p>
+            <p>Total Price: {totalPrice}</p>
             <p>Total Shipping: {totalShipping}</p>
             <p>Tax: {tax}</p>
             <h5>Grand Total: {GrandTotal.toFixed(2)}</h5>
