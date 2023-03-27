8f7239fa9c6c17cf2e907331bf9f179811ecd9d4
Prosantaroy1 <royprosanta172@gmail.com>
2023-03-27 20:17:29 +0600

    card desing completed

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    //console.log(props.product)
-    const {name, img, seller, price, quantity} = props.product;
+    console.log(props.product)
+    const {name, img, seller, price, ratings} = props.product;
     return (
         <div className='product'>
             <img src={img ? img: 'product-img'} alt='' />
-            <h2 className='title-name'>name: {name}</h2>
-            <p>Price: ${price}</p>
+            <div className='card-details'>
+                <h2 className='title-name'>Name: {name}</h2>
+                <p className='price-title'>Price: ${price}</p>
+                <p className='company-title price-title'>Company: {seller}</p>
+                <p className='price-title'>Rating: {ratings} star</p>
+            </div>
+            <button className='btn-add'>Add Card</button>
         </div>
     );
 };
